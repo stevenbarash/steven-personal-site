1f97e7948f0715cd2b3430b9c4abc515cc8f445b
Steven Barash <stevenbarash6@gmail.com>
2024-02-03 12:22:22 -0500

    rollback

--- src/pages/PhotoPage.jsx
+++ src/pages/PhotoPage.jsx
@@ -3,11 +3,12 @@ import InstagramEmbed from "../components/InstagramEmbed"
 import { useLocation } from 'react-router-dom';
 
 const PhotoPage = () => {
-  const location = useLocation();
+  // const location = useLocation();
   return (
-    <div key={location.key}>
-      <InstagramEmbed url="https://www.instagram.com/steven.photography"/>
-    </div>
+    <h1>test</h1>
+    // <div key={location.key}>
+    //   <InstagramEmbed url="https://www.instagram.com/steven.photography"/>
+    // </div>
   );};
 
 export default PhotoPage;
