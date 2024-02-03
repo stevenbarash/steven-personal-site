906dee90b46d841a4b91c4293f430306f82edab0
Steven Barash <stevenbarash6@gmail.com>
2024-02-03 18:49:52 -0500

    fix missing page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,49 +1,57 @@
-import fs from 'fs';
-import path from 'path';
-import Link from 'next/link';
-import MenuBar from '../components/menubar'
-export default function Blog({ posts }) {
+// import fs from 'fs';
+// import path from 'path';
+// import Link from 'next/link';
+// import MenuBar from '../components/menubar'
+// export default function Blog({ posts }) {
+//     return (
+//         <div>
+//             <MenuBar/>
+//             <h1>Blog Posts</h1>
+//             <ul>
+//                 {posts.map((post) => (
+//                     <li key={post.slug}>
+//                         <Link href={`/posts/${post.slug}`}>
+//                             <a>{post.title}</a>
+//                         </Link>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export async function getStaticProps() {
+//     const postsDirectory = path.join(process.cwd(), 'Posts');
+//     const filenames = fs.readdirSync(postsDirectory);
+
+//     const posts = filenames.map((filename) => {
+//         const filePath = path.join(postsDirectory, filename);
+//         const fileContents = fs.readFileSync(filePath, 'utf8');
+
+//         // Assuming the markdown files have a front matter with a "title" field
+//         const match = fileContents.match(/title:\s*(.*)/);
+//         const title = match && match[1] ? match[1] : '';
+
+//         // Extracting the slug from the filename
+//         const slug = filename.replace(/\.md$/, '');
+
+//         return {
+//             title,
+//             slug,
+//         };
+//     });
+
+//     return {
+//         props: {
+//             posts,
+//         },
+//     };
+// }
+
+export default function Blog() {
     return (
         <div>
-            <MenuBar/>
-            <h1>Blog Posts</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.slug}>
-                        <Link href={`/posts/${post.slug}`}>
-                            <a>{post.title}</a>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <h1>Under Construction</h1>
         </div>
     );
-}
-
-export async function getStaticProps() {
-    const postsDirectory = path.join(process.cwd(), 'Posts');
-    const filenames = fs.readdirSync(postsDirectory);
-
-    const posts = filenames.map((filename) => {
-        const filePath = path.join(postsDirectory, filename);
-        const fileContents = fs.readFileSync(filePath, 'utf8');
-
-        // Assuming the markdown files have a front matter with a "title" field
-        const match = fileContents.match(/title:\s*(.*)/);
-        const title = match && match[1] ? match[1] : '';
-
-        // Extracting the slug from the filename
-        const slug = filename.replace(/\.md$/, '');
-
-        return {
-            title,
-            slug,
-        };
-    });
-
-    return {
-        props: {
-            posts,
-        },
-    };
 }
