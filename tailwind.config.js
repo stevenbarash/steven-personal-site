c76ce96141d0b8a136adff3cc7fcd6a72ba2987a
Steven Barash <stevenbarash6@gmail.com>
2024-10-09 11:22:14 -0400

    remove unused page

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,6 @@ module.exports = {
         sans: ['Inter', 'sans-serif'],
         serif: ['Merriweather', 'serif'],
       },
-      colors: {
-        // ... (other colors)
-        offwhite: '#F5F5F5', // You can adjust this hex code to your preferred shade of off-white
-      },
     },
   },
   plugins: [
